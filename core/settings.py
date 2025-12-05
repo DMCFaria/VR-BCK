31841e300a0393cfd9e15a8ad81104030309d7bb
Daniel Mello <danielmello0110@gmail.com>
2025-12-05 10:01:55 -0300

    port fix

--- core/settings.py
+++ core/settings.py
@@ -132,8 +132,8 @@ SIMPLE_JWT = {
     'USER_ID_CLAIM': 'user_id',
 }
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
